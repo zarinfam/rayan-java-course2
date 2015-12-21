f3798c172daabbf2a3dd221112fa715abb26f10a
zarinfam.s <zarinfam.s@gmail.com>
2015-12-21 15:40:01 +0330

    add ! at the end of hello message.

--- src/main/java/com/rayan/course/java/Main.java
+++ src/main/java/com/rayan/course/java/Main.java
@@ -6,7 +6,7 @@ package com.rayan.course.java;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world!");
     }
 
 }
